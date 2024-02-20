37530b6df768a0e9d71abeb846849075a8dd76b7
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-02-20 10:59:12 +0530

    final changes to folder

--- Project/Chess/script.js
+++ Project/Chess/script.js
@@ -7,9 +7,8 @@ const player1=document.querySelector('.player--1');
 const btnroll=document.querySelector('.btn--roll');
 const btnhold=document.querySelector('.btn--hold');
 
-let currentscore0=0;
-let currentscore1=0;
-let activeplayer=0;
+let currentscore=0;
+let activeplayer=0; //o or 1
 let score;
 
 let start=function()
@@ -17,18 +16,16 @@ let start=function()
 score=[0,0];
 score0.textContent=0;
 score1.textContent=0;
-dice.classList.add('hidden')
+dice.classList.add('hidden');
 }
 start();
-
-const switchplayer=function()
+function switchPlayer()
 {
-
-    document.getElementById(`current--${activeplayer}`).textContent=0;
-    currentscore0=0;
-    activeplayer=activeplayer===0?1:0;
-    player0.classList.toggle('player--active');
-    player1.classList.toggle('player--active');
+currentscore=0;
+document.querySelector(`#current--${activeplayer}`).textContent=currentscore;
+document.querySelector(`.player--${activeplayer}`).classList.remove('player--active')
+activeplayer=activeplayer==0?1:0;
+document.querySelector(`.player--${activeplayer}`).classList.add('player--active');
 
 }
 
@@ -36,14 +33,24 @@ btnroll.addEventListener('click',function(){
 
 const imgurl=Math.trunc(Math.random()*6+1);
 dice.classList.remove('hidden');
-dice.src=`dice-${imgurl}.png`;
+dice.src=`dice-${imgurl}.png`
 
 if(imgurl!=1)
 {
-    currentscore0+=imgurl;
+currentscore+=imgurl;
+document.querySelector(`#current--${activeplayer}`).textContent=currentscore;
 }
+else{
+    switchPlayer();
+}
+})
 
+btnhold.addEventListener('click',function()
+{
 
+score[activeplayer]+=currentscore;
+document.querySelector(`#score--${activeplayer}`).textContent=score[activeplayer];
+switchPlayer();
 
 
 })
