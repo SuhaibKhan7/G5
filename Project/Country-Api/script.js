7c62c6976927e6f32b8a180568e4b54d3746dfec
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-02-28 10:17:02 +0530

    js updated

--- Project/Country-Api/script.js
+++ Project/Country-Api/script.js
@@ -4,32 +4,37 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-
-const request=new XMLHttpRequest();
-request.open('GET',`https://restcountries.com/v3.1/name/india`);
-request.send();
-
-request.addEventListener('load',function()
+function getCountry(country)
 {
-//    const data= JSON.parse(this.responseText);
-//    console.log(data);
+  const request=new XMLHttpRequest();
+  request.open('GET',`https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  
+  request.addEventListener('load',function(){
+  const data=JSON.parse(this.responseText)[1]||JSON.parse(this.responseText)[0];
+  console.log(data);
+  let languages=''
+  for(lan in data.languages)
+  {
+  languages+=data.languages[lan]+' ';
+  
+  }
+  console.log(languages)
+  const card=` <article class="country">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${Math.floor((data.population)/1000000)}M</p>
+    <p class="country__row"><span>🗣️</span> ${languages}</p>
+    <p class="country__row"><span>💰</span>CUR</p>
+  </div>
+  </article>
+  `
+  countriesContainer.insertAdjacentHTML('beforeend',card);
 
-console.log('DATA:');
-const data=JSON.parse(this.responseText)[1]
- console.log(data);
-
-const html=`  <article class="country">
-<img class="country__img" src="" />
-<div class="country__data">
-  <h3 class="country__name"></h3>
-  <h4 class="country__region">REGION</h4>
-  <p class="country__row"><span>👫</span>POP people</p>
-  <p class="country__row"><span>🗣️</span>LANG</p>
-  <p class="country__row"><span>💰</span>CUR</p>
-</div>
-</article>
-`
+  })
 
 
-
-})
+}  
+getCountry('russia')
