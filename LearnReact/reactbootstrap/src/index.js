5e8dd5bfdee01e9c493e5f6fef6cffe9a2435885
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-04-04 12:24:00 +0530

    G5 learning react boot strap

---
+++ LearnReact/reactbootstrap/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './component/Header';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<div>
+  <Header/>
+  <h1>This is first App</h1>
+</div>
+);
+
