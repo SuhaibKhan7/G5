6678bb709dd70b207ee26bafbd8bcc9ad3f274bb
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-04-18 10:21:52 +0530

    useHoook

--- LearnReact/reactbootstrap/src/component/LearnUseEffect.js
+++ LearnReact/reactbootstrap/src/component/LearnUseEffect.js
@@ -1,24 +1,31 @@
 import React, { useEffect, useState } from 'react'
 
 export default function LearnUseEffect() {
-    const[counter,setCounter]=useState(0)
-       console.log("Enter◀︎")
+  const [counter, setCounter] = useState(0)
+  const [data, setData] = useState([])
 
-        useEffect( ()=>{
 
-        console.log("Mount"+counter)
 
-        return ()=>{
-            console.log("unmount"+counter)
-        }
+  useEffect(() => {
+    fetch('http://localhost:3004/products')
+      .then(response => response.json())
+      .then(result => {
+        setData(result)
 
-        },[counter])
-
+      }
+      )
+  }, [])
   return (
     <div>
-    <h1>counter:{counter}</h1>
-    <button onClick={()=>setCounter(counter+1)}>Inc</button>
+      <h1>counter:{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Inc</button>
+      {console.log(data)}
 
+      <div className='bg-success text-white'>
+        {data.map((i) =>
+          <li key={i.id}>{i.id}----{i.pname}---------{i.price}---<span className={JSON.parse(i.instock)?'bg-primary':'bg-danger'}>{JSON.parse(i.instock)?'Instock':'OutofStock'}</span>   </li>
+        )}
+      </div>
     </div>
 
 
