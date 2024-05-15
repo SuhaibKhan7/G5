6d100f371c837b3baedec060b067ebfc0f333e33
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-05-16 01:00:23 +0530

    UPDATED

--- React/mongodb/src/components/Singup.js
+++ React/mongodb/src/components/Singup.js
@@ -2,34 +2,31 @@ import React, { useState } from 'react'
 
 export default function Singup() {
     const [formdata, setFormdata] = useState({
-        username: ''
+        username: '',
+        password:''
 
     })
 
     async function getinfo() {
         const response = await fetch("http://localhost:5000/data")
         console.log(response);
-        const data = await response.json();
+        const data = await response.text();
         console.log(data);
     }
     async function senddata(e) {
-        try {
-            const response = await fetch("http://localhost:5000/data", {
-                method: 'POST',
-                body: JSON.stringify(formdata),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            
-            console.log(response);
-            const data = await response.json();
-            console.log(data);
+        e.preventDefault();
+        const response = await fetch("http://localhost:5000/data", {
+            method: 'POST',
+            body: JSON.stringify(formdata),
+            headers: {
+                'Content-Type': 'application/json'
+            }
 
-        }
-        catch (error) {
-            console.log(error)
-        }
+        })
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+
     }
 
 
@@ -40,10 +37,11 @@ export default function Singup() {
 
     return (
         <div>
-
-            <form action="">
+            <form action="" onSubmit={senddata}>
                 <input type="text" name='username' onChange={handleinput} />
-                <button onClick={senddata}>save</button>
+                <input type="password" name='password' onChange={handleinput} />
+                
+                <button type='submit'>Singup</button>
             </form>
 
 
--- React/nodeserver/index.js
+++ React/nodeserver/index.js
@@ -1,17 +1,46 @@
 const express = require('express');
 const cors = require('cors')
-const server = express();
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb+srv://mycluster:7eSQxScZMaKQBZUq@atlascluster.xlm0rrn.mongodb.net/');
+
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password:String
+
+});
+const User = mongoose.model('Userdata', userSchema);
+
+
+
+
+
+
+const server = express();
 server.use(cors())
+server.use(express.json())
+
+
+
 
 server.get('/data', (req, res) => {
-
-    res.send({ username: 'help' });
-
+    res.send('hi');
 })
 server.post('/data', (req, res) => {
-    req.send(req)
-    console.log(req.body)
+
+    const user=new User();
+    user.username=req.body.username;
+    user.password=req.body.password
+    user.save();
+    console.log(req.body.username)
+    res.send({ msg: "Data rec" })
 })
 
 
