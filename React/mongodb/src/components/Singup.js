355a59673cb621c3c9a6e181dc110322f5d62158
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-05-16 10:24:48 +0530

    login auth

--- React/mongodb/src/components/Singup.js
+++ React/mongodb/src/components/Singup.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 export default function Singup() {
     const [formdata, setFormdata] = useState({
         username: '',
-        password:''
+        password: ''
 
     })
 
@@ -13,6 +13,8 @@ export default function Singup() {
         const data = await response.text();
         console.log(data);
     }
+
+
     async function senddata(e) {
         e.preventDefault();
         const response = await fetch("http://localhost:5000/data", {
@@ -29,22 +31,48 @@ export default function Singup() {
 
     }
 
-
     function handleinput(e) {
         setFormdata({ ...formdata, [e.target.name]: e.target.value })
     }
 
+
+
+
+async function handlelogin(e) {
+        e.preventDefault();
+        try {
+
+            const response = await fetch("http://localhost:5000/login", {
+                method: 'POST',
+                body: JSON.stringify(formdata),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            )
+            console.log(response);
+            const data = await response.json();
+            console.log(data);
+        }
+        catch (error) {
+            console.log('Login not successfull')
+        }
+
+    }
 
     return (
         <div>
             <form action="" onSubmit={senddata}>
                 <input type="text" name='username' onChange={handleinput} />
                 <input type="password" name='password' onChange={handleinput} />
-                
+
                 <button type='submit'>Singup</button>
             </form>
 
-
+            <form action="" onSubmit={handlelogin}>
+                <input type="text" name='username' onChange={handleinput} />
+                <button type='submit'>Login</button>
+            </form>
 
 
             <button onClick={getinfo}>getinfo</button>
--- React/nodeserver/index.js
+++ React/nodeserver/index.js
@@ -3,24 +3,21 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 
+
+
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb+srv://mycluster:7eSQxScZMaKQBZUq@atlascluster.xlm0rrn.mongodb.net/');
+    await mongoose.connect('mongodb+srv://singup:rdgeX2uuNPRyARne@cluster67376.kmci2em.mongodb.net/?retryWrites=true&w=majority&appName=Cluster67376');
 
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 const userSchema = new mongoose.Schema({
     username: String,
-    password:String
-
+    password: String
 });
-const User = mongoose.model('Userdata', userSchema);
-
-
-
-
+const User = mongoose.model('userdata', userSchema);
 
 
 const server = express();
@@ -34,14 +31,33 @@ server.get('/data', (req, res) => {
     res.send('hi');
 })
 server.post('/data', (req, res) => {
-
-    const user=new User();
-    user.username=req.body.username;
-    user.password=req.body.password
+    const user = new User();
+    user.username = req.body.username,
+        user.password = req.body.password
     user.save();
+
+
     console.log(req.body.username)
     res.send({ msg: "Data rec" })
 })
+server.post('/login', async (req, res) => {
+    res.send({ msg: 'under login' })
+    const user = new User();
+    const loginname = req.body.username;
+
+
+    const validuser = await user.find({ username: loginname })
+
+    if (validuser) {
+        res.send({ msg: 'loginsuccessfull' })
+    }
+    else {
+        res.send({ msg: 'login unsuccessfull' })
+    }
+
+})
+
+
 
 
 
